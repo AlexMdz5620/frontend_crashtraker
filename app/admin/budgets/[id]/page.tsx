3e22b5e9c04281ec8010651f46d4cac3b408e58e
Manu Mdz <manuel.mendozac.dev@gmail.com>
2025-08-17 10:02:47 -0600

    Admin - Graficando Porcentajes

--- app/admin/budgets/[id]/page.tsx
+++ app/admin/budgets/[id]/page.tsx
@@ -1,5 +1,7 @@
+import ProgressBar from '@/components/budget/ProgressBar';
 import AddExpenseButton from '@/components/expense/AddExpenseButton';
 import ExpenseMenu from '@/components/expense/ExpenseMenu';
+import Amount from '@/components/ui/Amount';
 import ModalContainer from '@/components/ui/ModalContainer';
 import { getBudget } from '@/src/services/budgets';
 import { formatCurrency, formatDate } from '@/src/utlis';
@@ -15,12 +17,17 @@ export async function generateMetadata({ params }: { params: { id: string } }): 
 
 export default async function BudgetDetailsPage({ params }: { params: { id: string } }) {
     const budget = await getBudget(params.id);
+
+    const totalSpent = budget.expenses.reduce((total, expense) => +expense.amount + total, 0);
+    const totalAvailabe = +budget.amount - totalSpent;
+
+    const percentage = +((totalSpent / +budget.amount) * 100).toFixed(2);
     
     return (
         <>
             <div className='flex justify-between items-center'>
                 <div>
-                    <h1 className="font-black text-4xl text-purple-950">{budget.name}</h1>
+                    <h1 className="font-black text-4xl text-purple-800">{budget.name}</h1>
                     <p className="text-xl font-bold">Administra tus {''} <span className="text-amber-500">gastos</span></p>
                 </div>
                 <AddExpenseButton />
@@ -28,6 +35,26 @@ export default async function BudgetDetailsPage({ params }: { params: { id: stri
             {budget.expenses.length
                 ? (
                     <>
+                        <div className='grid grid-cols-1 md:grid-cols-2 m-10'>
+                            <ProgressBar
+                                percentage={percentage}
+                            />
+                            <div className='flex flex-col justify-center items-center md:items-start gap-5'>
+                                <Amount
+                                    label='Presupuesto'
+                                    amount={+budget.amount}
+                                />
+                                <Amount
+                                    label='Disponible'
+                                    amount={totalAvailabe}
+                                />
+                                <Amount
+                                    label='Gastado'
+                                    amount={totalSpent}
+                                />
+                            </div>
+                        </div>
+
                         <h1 className='font-black text-4xl text-purple-800'>
                             Gastos en este presupuesto
                         </h1>
